0f4d6dc79f5b9c2b2e47da1b02d1fd7b141aef09
nikuzuki <nikuzuki29@gmail.com>
2018-06-21 22:03:22 +0900

    seedsに時刻表のデータを入れた(うなすけさんにリファクタリングしてもらった)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+["6:40",
+"7:20",
+"8:00",
+"8:40",
+"9:15",
+"9:55",
+"10:30",
+"11:10",
+"11:50",
+"12:30",
+"13:10",
+"13:50",
+"15:00",
+"16:00",
+"16:30",
+"17:00",
+"17:30",
+"18:10",
+"18:50",
+"19:30",
+"20:10",
+"20:50",
+"21:30",
+"22:05",
+"22:45",
+"23:15"].each do |d|
+  JaistBusTimeTableInWeekday.find_or_create_by(starttime: Time.parse("2000/01/01 #{d}"))
+end
