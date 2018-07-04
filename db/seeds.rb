f8d4f7de0bbfd6b468bd7cc3f87caf4378abad70
nikuzuki <nikuzuki29@gmail.com>
2018-07-04 20:34:25 +0900

    seedsに休日のタイムテーブルを追加した

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# 平日のタイムテーブル
 ["6:40",
 "7:20",
 "8:00",
@@ -33,4 +34,31 @@
 "22:45",
 "23:15"].each do |d|
   JaistBusTimeTableInWeekday.find_or_create_by(starttime: Time.zone.parse("2000/01/01 #{d}"))
+end
+
+# 休日のタイムテーブル
+["7:20",
+"8:00",
+"8:40",
+"9:15",
+"9:55",
+"10:30",
+"11:10",
+"11:50",
+"12:30",
+"13:10",
+"13:50",
+"15:00",
+"16:00",
+"16:50",
+"17:30",
+"18:10",
+"18:50",
+"19:30",
+"20:10",
+"20:50",
+"21:30",
+"22:05",
+"22:45"].each do |d|
+  JaistBusTimeTableInHoliday.find_or_create_by(starttime: Time.zone.parse("2000/01/01 #{d}"))
 end
